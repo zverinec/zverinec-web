850d5a41d484381d5b2355e0f888127842b30f0f
Jan Horacek <me@apophis.cz>
2019-12-11 21:54:28 +0100

    Add git autodeploy.

---
+++ deploy.php
@@ -0,0 +1,13 @@
+<?php
+require_once("deploy-token.php");
+
+define("REMOTE_REPOSITORY", "https://github.com/zverinec/zverinec-web.git");
+define("DIR", ".");
+define("BRANCH", "refs/heads/master");
+define("LOGFILE", "/dev/null");
+define("GIT", "git");
+define("MAX_EXECUTION_TIME", 180);
+define("BEFORE_PULL", "git reset --hard @{u}");
+define("AFTER_PULL", "");
+
+require_once("git-deploy/deployer.php");
